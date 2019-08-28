5f5028ddf687465fc3c7daa03216a20fea733b3d
ZumbaIdol <david.a.foote@gmail.com>
2019-08-28 17:41:45 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,11 +4,23 @@ class Show < ActiveRecord::Base
     Show.maximum(:rating)
   end
 
+  def Show::most_popular_show
+    Show.order(rating: :desc).first
+  end
+
   def Show::ratings_sum
     Show.sum(:rating)
   end
 
   def Show::lowest_rating
     Show.minimum(:rating)
+  end
+
+  def Show::least_popular_show
+    Show.order(rating: :asc).first
+  end
+
+  def Show::popular_shows
+    Show.where("rating > 5")
   end
 end
