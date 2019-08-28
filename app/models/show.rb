b9a8c9a6877e7c7ce9f939543ba6973859e58bf2
ZumbaIdol <david.a.foote@gmail.com>
2019-08-28 17:27:34 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,14 @@
 class Show < ActiveRecord::Base
+
+  def Show::highest_rating
+    Show.maximum(:rating)
+  end
+
+  def Show::ratings_sum
+    Show.sum(:rating)
+  end
+
+  def Show::lowest_rating
+    Show.minimum(:rating)
+  end
 end
